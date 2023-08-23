81b4255fc27d455bff02ac937f55b937d4894e9b
Aditya <dev.adityasus@gmail.com>
2023-08-23 11:45:14 +0530

    Builds basic screen with a logo and a title

--- src/JotEase/JotEase.java
+++ src/JotEase/JotEase.java
@@ -1,8 +1,26 @@
 package JotEase;
 
-public class JotEase {
+import javax.swing.*;
+
+import java.awt.*;
+
+public class JotEase extends JFrame {
+
+    JotEase(){
+        setTitle("Note");
+
+//        Set Logo
+        ImageIcon JotEaseLogo = new ImageIcon(ClassLoader.getSystemResource("JotEase/icons/notepad.png"));
+        Image logo = JotEaseLogo.getImage();
+        setIconImage(logo);
+
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setVisible(true);
+    }
 
     public static void main(String[] args) {
+
+        new JotEase();
 
     }
 }
